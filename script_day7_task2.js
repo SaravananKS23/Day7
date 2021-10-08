b0fcee48460aa1c15fd7e59c52538facd03f4833
SaravananKS23 <91030871+SaravananKS23@users.noreply.github.com>
2021-10-09 03:13:05 +0530

    Add files via upload

---
+++ script_day7_task2.js
@@ -0,0 +1,14 @@
+//1. Create XHR object/constructor
+var request=new XMLHttpRequest();
+//2. Open a connection
+request.open("GET","https://restcountries.com/v2/all",true);
+//3. Initiate the connection
+request.send();
+//4. Once the data loaded from the server successfully
+request.onload=function(){
+//Here we are converting "string" to array of object json
+var result=JSON.parse(request.response); 
+var regions = result.filter((ele)=> ele.population <200000);
+let output=regions.filter(fin => console.log(fin.name));
+console.log(output);
+}
---
+++ script_day7_task5.js
@@ -0,0 +1,28 @@
+//1. Create XHR object/constructor
+var request=new XMLHttpRequest();
+//2. Open a connection
+request.open("GET","https://restcountries.com/v2/all",true);
+//3. Initiate the connection
+request.send();
+//4. Once the data loaded from the server successfully
+request.onload=function(){
+//Here we are converting "string" to array of object json
+var result=JSON.parse(request.response); 
+//var currency = result.filter(element=> console.log(element.currencies));
+var currency = result.filter(ele=>
+    
+    {
+        try{
+            if (ele.currencies === undefined)
+        {
+            throw new Error("No currency for");
+        }
+        if ((ele.currencies[0].code==="USD")===true)
+        {
+            console.log(ele.name+": "+ele.currencies[0].code);
+        }
+        }catch(e){
+        console.log(e.message+" "+ele.name);
+        }
+    })
+}
